c76b86e1d908c2bbac2944af6a487ccba4d582ee
qiyu6 <qiyu6@illinois.edu>
2024-08-04 00:33:26 -0500

    update exps

--- exps/hparam1.bat
+++ exps/hparam1.bat
@@ -1,2 +1,15 @@
 @REM foursqure-twitter
-python main.py --dataset=foursqure-twitter --runs=3 --lambda_edge=40 --lambda_total=1e-3
+python main.py --dataset=foursqure-twitter --runs=3 --lambda_edge=40 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3
+python main.py --dataset=foursqure-twitter --runs=3 --lambda_edge=40 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4
+
+python main.py --dataset=foursqure-twitter --runs=3 --lambda_edge=50 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3
+python main.py --dataset=foursqure-twitter --runs=3 --lambda_edge=50 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4
+
+python main.py --dataset=foursqure-twitter --runs=3 --lambda_edge=60 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3
+python main.py --dataset=foursqure-twitter --runs=3 --lambda_edge=60 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4
+
+python main.py --dataset=foursqure-twitter --runs=3 --lambda_edge=70 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3
+python main.py --dataset=foursqure-twitter --runs=3 --lambda_edge=70 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4
+
+python main.py --dataset=foursqure-twitter --runs=3 --lambda_edge=80 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3
+python main.py --dataset=foursqure-twitter --runs=3 --lambda_edge=80 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4
