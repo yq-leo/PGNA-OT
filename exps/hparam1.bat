29a4c3e1375694a6908e6131a264c7b32047470f
Leo Yu <qiyuleo@outlook.com>
2024-08-06 23:29:22 -0500

    upload res

--- exps/hparam1.bat
+++ exps/hparam1.bat
@@ -1,18 +1,29 @@
 @REM foursquare-twitter
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=40 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=40 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=40 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=40 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
+@REM
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=50 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=50 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
+@REM
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=60 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=60 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
+@REM
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=65 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=65 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
+@REM
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
+@REM
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=80 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=80 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
 
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=50 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=50 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=1e-3 --lambda_w=1 --lr=3e-4 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=1e-3 --lambda_w=1 --lr=1e-4 --epochs=20
+@REM
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=5e-4 --lambda_w=1 --lr=5e-4 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=5e-4 --lambda_w=1 --lr=3e-4 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=5e-4 --lambda_w=1 --lr=1e-4 --epochs=20
 
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=60 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=60 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
-
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=65 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=65 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
-
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
-
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=80 --lambda_total=1e-3 --lambda_w=1 --lr=1e-3 --epochs=20
-python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=80 --lambda_total=1e-3 --lambda_w=1 --lr=5e-4 --epochs=20
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=5e-4 --lambda_w=1 --lr=3e-4 --epochs=40
+@REM python main.py --dataset=foursquare-twitter --runs=3 --lambda_edge=70 --lambda_total=5e-4 --lambda_w=1 --lr=1e-4 --epochs=60
---
+++ exps/hparam2.bat
@@ -0,0 +1,33 @@
+@REM ACM-DBLP
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=30 --lambda_edge=500 --lambda_w=0.5 --lambda_total=5e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=30 --lambda_edge=550 --lambda_w=0.5 --lambda_total=5e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=30 --lambda_edge=600 --lambda_w=0.5 --lambda_total=5e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=30 --lambda_edge=650 --lambda_w=0.5 --lambda_total=5e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=30 --lambda_edge=700 --lambda_w=0.5 --lambda_total=5e-3
+
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1000 --lambda_w=0.5 --lambda_total=5e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1000 --lambda_w=0.5 --lambda_total=3e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1000 --lambda_w=0.5 --lambda_total=1e-3
+@REM
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1500 --lambda_w=0.5 --lambda_total=5e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1500 --lambda_w=0.5 --lambda_total=3e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1500 --lambda_w=0.5 --lambda_total=1e-3
+@REM
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=2000 --lambda_w=0.5 --lambda_total=5e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=2000 --lambda_w=0.5 --lambda_total=3e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=2000 --lambda_w=0.5 --lambda_total=1e-3
+@REM
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=2500 --lambda_w=0.5 --lambda_total=5e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=2500 --lambda_w=0.5 --lambda_total=3e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=2500 --lambda_w=0.5 --lambda_total=1e-3
+
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=900 --lambda_w=0.5 --lambda_total=5e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=950 --lambda_w=0.5 --lambda_total=5e-3
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1000 --lambda_w=0.5 --lambda_total=5e-3
+
+@REM python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1050 --lambda_w=0.8 --lambda_total=5e-3 --lr=5e-4
+python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1100 --lambda_w=0.8 --lambda_total=5e-3 --lr=5e-4
+python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1150 --lambda_w=0.8 --lambda_total=5e-3 --lr=5e-4
+python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1200 --lambda_w=0.8 --lambda_total=5e-3 --lr=5e-4
+python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1250 --lambda_w=0.8 --lambda_total=5e-3 --lr=5e-4
+python main.py --dataset=ACM-DBLP --runs=1 --epochs=20 --lambda_edge=1300 --lambda_w=0.8 --lambda_total=5e-3 --lr=5e-4
